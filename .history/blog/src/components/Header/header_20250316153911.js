ae6752ee96bd2bc86de561a4538866073525b9c8
Saw Paing <sawp2489@gmail.com>
2025-04-06 14:38:58 -0500

    Completed Activity 7

--- .history/blog/src/components/Header/header_20250316153911.js
+++ .history/blog/src/components/Header/header_20250316153911.js
@@ -1,8 +1,6 @@
 import * as React from "react"
 import { Link } from "gatsby"
 import styled from "styled-components"
-import { H1 } from "../Heading"
-import { Section } from "../Section"
 
 const StyledHeader = styled.header`
   margin: 0 auto;
@@ -21,17 +19,10 @@ const StyledLink = styled(Link)`
 
 const Header = ({ siteTitle }) => (
   <StyledHeader>
-    <Section width={11/12}>
     <StyledLink to="/">
-      <H1>
-        {siteTitle}
-      </H1>
-    </StyledLink>   
-    </Section>
-    <Section width={1/12}>
-      Search
-    </Section>
+      {siteTitle}
+    </StyledLink>
   </StyledHeader>
 )
 
-export { Header }
+export default Header
---
+++ .history/blog/src/components/Heading/Heading1_20250406143322.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import { BaseHeading } from './BaseHeading'
+
+const Heading1 = props => 
+  <BaseHeading fontSize={[3, 4, 5]} {...props} />
+
+export { Heading1 as H1 }
